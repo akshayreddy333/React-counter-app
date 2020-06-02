4f8b208fa75ed2c154af4219dd388af3079c7c5c
akshayreddy333 <akshaymunna95@gmail.com>
2020-06-02 18:39:14 -0400

    event handling

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,29 +3,39 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0,
-    tags: [],
   };
 
-  renderTags() {
-    if (this.state.tags.length === 0) return <p> There are no tags!</p>;
+  //   constructor() {
+  //     super();
+  //     this.handleIncrement = this.handleIncrement.bind(this);
+  //   }
 
-    return (
-      <ul>
-        {/* The key should be unique for virtual DOM to easily identify the changes */}
-        {this.state.tags.map((tag) => (
-          <li key={tag}>{tag}</li>
-        ))}
-      </ul>
-    );
-  }
+  handleIncrement = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
 
   render() {
     return (
       <div>
-        {this.state.tags.length === 0 && "Please create a new tag!"}
-        {this.renderTags()}
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
       </div>
     );
+  }
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count == 0 ? "zero" : count;
   }
 }
 
