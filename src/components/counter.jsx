38e44b8d9ba15236eeee5565e10892063bc7741a
akshayreddy333 <akshaymunna95@gmail.com>
2020-06-02 17:14:54 -0400

    Rendering lists

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,15 +3,20 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0,
-    // imageUrl: "https://picsum.photos/200",
+    tags: ["tag1", "tag2", "tag3"],
   };
 
   render() {
     return (
       <div>
-        {/* <img src={this.state.imageUrl} alt="" /> */}
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button className="btn btn-secondary btn-sm">Increment</button>
+        <ul>
+          {/* The key should be unique for virtual DOM to easily identify the changes */}
+          {this.state.tags.map((tag) => (
+            <li key={tag}>{tag}</li>
+          ))}
+        </ul>
       </div>
     );
   }
